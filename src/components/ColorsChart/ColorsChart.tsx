07a2484104c04c2ae04fc83b6464666d068198d5
Gabriel Dominguez Duran <gabrieldominguezduran@gmail.com>
2023-05-15 15:04:12 +0200

    Add colors pie chart

--- src/components/ColorsChart/ColorsChart.tsx
+++ src/components/ColorsChart/ColorsChart.tsx
@@ -1,27 +1,43 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useState, useEffect } from "react";
-import * as d3 from "d3";
+import { arc, csv, DSVRowArray, pie, PieArcDatum } from "d3";
 
 const url =
   "https://gist.githubusercontent.com/gabrieldominguezduran/fb36bd88d19db61f960dd8af75f5018a/raw/colors.csv";
 
+const width = 960;
+const height = 500;
+const centerX = width / 2;
+const centerY = height / 2;
+
 const ColorsChart = () => {
-  const [data, setData] = useState({});
-  const [message, setMessage] = useState("");
+  const [data, setData] = useState<DSVRowArray<string>>();
+
+  const pieArc = arc().innerRadius(0).outerRadius(width);
+  const colorPie = pie().value(1);
+
   useEffect(() => {
-    d3.csv(url).then((data) => {
-      setMessage(`
-      Data: ${Math.round(d3.csvFormat(data).length / 1024)} KB
-       ${data.length} Rows 
-       ${data.columns.length} Columns`);
-      return setData(data);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    csv(url).then((data) => setData(data));
   }, []);
-  console.log("DATA: ", data);
+  if (!data) {
+    return <pre className="ColorsChart">Loading...</pre>;
+  }
+
   return (
-    <pre className="ColorsChart">
-      {Object.keys(data).length === 0 ? "Loading..." : message}
-    </pre>
+    <svg width={width} height={height}>
+      <g transform={`translate(${centerX}, ${centerY})`}>
+        {colorPie(data as any).map(
+          (d: PieArcDatum<number | { valueOf(): number }>) => (
+            <path
+              key={Math.random()}
+              fill={(d.data as { [key: string]: any })["hex_value"].toString()}
+              d={pieArc(d as any)!}
+            />
+          )
+        )}
+      </g>
+    </svg>
   );
 };
 
